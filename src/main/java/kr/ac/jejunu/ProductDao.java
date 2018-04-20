32eba1b0263229cade835cff9bcb99c2a169e776
voidblue <kimqwe12300@daum.net>
2018-04-21 00:14:15 +0900

    exam10, clean code

--- src/main/java/kr/ac/jejunu/ProductDao.java
+++ src/main/java/kr/ac/jejunu/ProductDao.java
@@ -5,14 +5,10 @@ import java.sql.*;
 
 public class ProductDao {
     private JdbcContext jdbcContext;
-    DataSource dataSource;
     public ProductDao(JdbcContext jdbcContext){
         this.jdbcContext = jdbcContext;
     }
 
-    public Connection getConnection() throws SQLException, ClassNotFoundException {
-        return dataSource.getConnection();
-    }
 
 
     public Product get(Long id) throws ClassNotFoundException, SQLException {
@@ -20,10 +16,6 @@ public class ProductDao {
         return jdbcContext.getContext(statementStrategy);
     }
 
-    private Product getContext(StatementStrategy statementStrategy) {
-
-        return jdbcContext.getContext(statementStrategy);
-    }
 
 
     public Long insert(Product product) throws SQLException, ClassNotFoundException {
@@ -31,21 +23,12 @@ public class ProductDao {
         return jdbcContext.insertContext(statementStrategy);
     }
 
-    private Long insertContext(StatementStrategy statementStrategy) {
-
-
-        return jdbcContext.insertContext(statementStrategy);
-    }
 
     public void update(Product product) {
         StatementStrategy statementStrategy = new UpdateStatementStategy(product);
         jdbcContext.updateContext(statementStrategy);
     }
 
-    private void updateContext(StatementStrategy statementStrategy) {
-
-        jdbcContext.updateContext(statementStrategy);
-    }
 
     public void delete(Long id) {
         StatementStrategy statementStrategy = new DeleteStatementStrategy(id);

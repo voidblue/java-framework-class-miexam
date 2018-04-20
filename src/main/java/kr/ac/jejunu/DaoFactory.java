004a5d47fca87166c4c90c7842c9b0dcd74ebb07
voidblue <kimqwe12300@daum.net>
2018-04-20 15:45:49 +0900

    exam6

--- src/main/java/kr/ac/jejunu/DaoFactory.java
+++ src/main/java/kr/ac/jejunu/DaoFactory.java
@@ -1,11 +1,28 @@
 package kr.ac.jejunu;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Driver;
 
 @Configuration
 public class DaoFactory {
+    @Value("${db.classname}")
+    private String classname;
+    @Value("${db.url}")
+    private String url;
+    @Value("${db.username}")
+    private String username;
+    @Value("${db.password}")
+    private String password;
+
+
+
+
     @Bean
     public ProductDao getDao() {
         return new ProductDao(connectionMaker());
@@ -13,5 +30,20 @@ public class DaoFactory {
     @Bean
     ConnectionMaker connectionMaker(){
         return new JejuConnectionMaker();
+    }
+
+    @Bean
+    DataSource dataSource(){
+        DataSource dataSource = new SimpleDriverDataSource();
+        try {
+            ((SimpleDriverDataSource) dataSource).setDriverClass((Class<? extends Driver>) Class.forName(classname));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ((SimpleDriverDataSource) dataSource).setUrl(url);
+        ((SimpleDriverDataSource) dataSource).setUsername(username);
+        ((SimpleDriverDataSource) dataSource).setPassword(password);
+        return dataSource;
     }
 }
